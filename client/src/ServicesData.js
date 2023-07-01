a5820bbf0ef0be40c338fc6562246a3e8c2724b3
sulogna2001 <roysulogna2001@gmail.com>
2023-07-02 00:05:17 +0530

    Added major files

---
+++ client/src/ServicesData.js
@@ -0,0 +1,17 @@
+export const services = [
+  {
+    name: "Secure Online Banking",
+    description:
+      "Experience hassle-free banking with our state-of-the-art online platform. Enjoy secure access to your accounts, make instant transfers, pay bills, and manage your finances anytime, anywhere. Our robust encryption and multi-factor authentication ensure the utmost security for your transactions.",
+  },
+  {
+    name: "Intelligent Financial Planning Tools",
+    description:
+      "Take control of your financial goals with our powerful planning tools. From budgeting and expense tracking to goal setting and investment analysis, our intuitive tools provide you with real-time insights and personalized recommendations to make informed financial decisions.",
+  },
+  {
+    name: "Streamlined Payment Solutions",
+    description:
+      "Simplify your payment processes with our seamless solutions. Whether it's peer-to-peer transfers, online payments, or merchant transactions, our secure and efficient payment gateway ensures quick and reliable transactions, saving you time and effort. Say goodbye to complex payment procedures and embrace a frictionless payment experience.",
+  },
+];
